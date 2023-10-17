68bfb7cd105d9e08ac3e04e6598b32cdf63ccd6d
pfb25 <pfb25@info25.cshl.edu>
2023-10-17 11:47:31 -0400

    first_python_script_PFB_2023)

---
+++ pythonproblemset1.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+import sys
+My_name = sys.argv
+My_favourite_color = sys.argv
+My_favourite_activity = sys.argv
+My_favourite_animal = sys.argv
+
+My_name ="Ayush"
+
+print(My_name)
+
+#added name to print
+
+print(f"My name: {My_name}")
+
+
+My_favourite_color ="Red"
+
+print("My favourite coloer is:",My_favourite_color)
+
+
+#added fav color 
+
+My_favourite_activity="not coding"
+
+
+print(My_favourite_activity)
+
+#added fav activity
+
+My_favourite_animal="chicken"
+
+print(My_favourite_animal)
+
+#My bad! This is shit!My_name ="Ayush",My_favourite_color ="Red",My_favourite_activity="not coding",My_favourite_animal="chicken"
+
+print(My_name,My_favourite_color,My_favourite_activity,My_favourite_animal)
+print(My_name+My_favourite_color+My_favourite_activity+My_favourite_animal)
