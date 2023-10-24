49aa16bfc9b8145a92ad26b2f0e7a5d0607b64f2
pfb25 <pfb25@info25.cshl.edu>
2023-10-23 21:14:01 -0400

    updated script

--- finding_fasta_header_using_patternmatch.py
+++ finding_fasta_header_using_patternmatch.py
@@ -10,6 +10,7 @@ my_seqs={}
 
 for line in my_file:
 	line=line.rstrip()
+	print(f"reading in {line}")
 
 #>gi|170746|gb|M12277.1|WHTH4091 Wheat histone H4 TH091 gene
 	
@@ -30,24 +31,25 @@ print(my_seqs)
 
 for seq_id in my_seqs:
 	sequence=my_seqs[seq_id]
-	#print(sequence)
+	print(sequence)
 	total=len(sequence)
-	#print('here 1')
+	print('here 1')
 
-	A_count=dna.count("A")
-	T_count=dna.count("T")
-	a_count=dna.count("a")
-	t_count=dna.count("t")
-	G_count=dna.count("G")
-	C_count=dna.count("C")
-	g_count=dna.count("g")
-	N_count=dna.count("N")
-	#print("here 2")
+	A_count=sequence.count("A")
+	T_count=sequence.count("T")
+	a_count=sequence.count("a")
+	t_count=sequence.count("t")
+	G_count=sequence.count("G")
+	C_count=sequence.count("C")
+	g_count=sequence.count("g")
+	N_count=sequence.count("N")
+	print("here 2")
 	
-	AT_content=(A_count+T_count)/(total)
+	#AT_content=(A_count+T_count)/(total)
 
 
-	print(f'total is {total}','A_count is {A_count}', 'a_count is {a_count}', 'Ncount is {N_count}')
+	print(f'total is; {total},A_count is; {A_count} , a_count is; {a_count} Ncount is; {N_count}')
 
+my_file.close()
 		
 
